a91c0851ae103d59aa5d44e80fd5dcb4e34cecba
Sitthata <aprils96950@gmail.com>
2024-10-14 23:51:43 +0700

    setting up and initial project

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold">Welcome to the home page</h1>
+    <Button @click="count++">Count: {{ count }}</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref<number>(0) ;
+</script>
